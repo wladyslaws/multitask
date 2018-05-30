63300e80eb5865c63bf6b8bbe76e947da2249f38
Wladyslaw Surala <wladek@tensorflight.com>
2018-05-30 13:04:12 +0200

    remove redundancy in multitask_utils

--- multitask_utils.py
+++ multitask_utils.py
@@ -1,21 +1,18 @@
+def print_value(value, key):
+    value_type = type(value)
+    if value_type is dict:
+        print_dict_types(value)
+    elif value_type is list:
+        print_list_types(value)
+    else:
+        print([(key, value_type)])
+
 def print_dict_types(dict_to_print):
     for key in dict_to_print.keys():
         value = dict_to_print[key]
-        value_type = type(value)
-        if value_type is dict:
-            print_dict_types(value)
-        elif value_type is list:
-            print_list_types(value)
-        else:
-            print([(key, value_type)])
+        print_value(value, key)
 
 def print_list_types(list_to_print: list):
     for value in list_to_print:
-        value_type = type(value)
-        if value_type is dict:
-            print_dict_types(value)
-        elif value_type is list:
-            print_list_types(value)
-        else:
-            print(value_type)
+        print_value(value, "in list")
 
